56fbced216f4fb5b49cd9986f62ca91b7c2be902
Prathmesh Sanjay Shevale <shevaleprathmesh8@gmail.com>
2022-03-24 09:41:09 +0530

    day 04(a)

--- Basic Programs/src/basics/AddTwoBinaryStrings.java
+++ Basic Programs/src/basics/AddTwoBinaryStrings.java
@@ -1,10 +1,31 @@
 package basics;
 
+import java.util.Scanner;
+
 public class AddTwoBinaryStrings {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc= new Scanner(System.in);
+		String a=sc.next();
+		String b=sc.next();
+		
+		int alen= a.length();
+		int blen= b.length();
+		
+		int i=0,carry=0;
+		String result="";
+		while(i<alen || i<blen || carry!=0) {
+			int x=0;
+			if(i<alen && a.charAt(alen-1-i)=='1') x=1;
+			int y=0;
+			if(i<blen && a.charAt(blen-1-i)=='1') y=1;
+			
+			result=(x+y+carry)%2+result;
+			carry=(x+y+carry)/2;
+			i++;
+		}
+		
+		System.out.println(result);
 	}
 
 }
